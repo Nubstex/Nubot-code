e50fbdc717e2e14f2b19937dd97fcd06795a8a87
Nubstex <91033840+Nubstex@users.noreply.github.com>
2021-09-22 21:37:53 +0200

    Update config.py

--- musicbot/config.py
+++ musicbot/config.py
@@ -37,7 +37,7 @@ class Config:
         self._confpreface = "An error has occured reading the config:\n"
         self._confpreface2 = "An error has occured validating the config:\n"
 
-        self._login_token = os.getenv['TOKEN']
+        self._login_token = os.environ['TOKEN']
 
         self.auth = ()
 
